577f3c343d2cacdf1f766294413c200b5934fea5
Las45 <matteo.morent@gmail.com>
2026-04-20 09:00:16 +0200

    gitignore

--- obj/Debug/net8.0-windows/App.g.cs
+++ obj/Debug/net8.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace Vokabeltrainer {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.10.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Vokabeltrainer {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.10.0")]
         public static void Main() {
             Vokabeltrainer.App app = new Vokabeltrainer.App();
             app.InitializeComponent();
--- obj/Debug/net8.0-windows/Vokabeltrainer.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Vokabeltrainer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vokabeltrainer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef8f1663c3c21934f6562ce3e623650bb860bbd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vokabeltrainer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vokabeltrainer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
